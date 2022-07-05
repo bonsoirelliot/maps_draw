37afc3fa24a48da2d04e4a3067724473642e6a72
bonsoirelliot <privetdek16@gmail.com>
2022-07-05 22:32:32 +0500

    начал делать

---
+++ android/app/src/main/java/dev/flutter/map_draw/MainActivity.java
@@ -0,0 +1,6 @@
+package dev.flutter.map_draw;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:map_draw/app.dart';
+
+void main() {
+  runApp(const MapDraw());
+}
